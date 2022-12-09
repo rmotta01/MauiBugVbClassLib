03deb573fefb2050bdbb0d8478c337ff95618ee2
rmotta <rodolfomottap@hotmail.com>
2022-12-09 11:22:44 -0300

    Add project files.

---
+++ MauiBugVbClassLib/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiBugVbClassLib;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ MauiBugVbClassLib/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiBugVbClassLib;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
